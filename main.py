6ad96c564afd6099b69482f2e6609775384c5115
chisoftltd <chisoftltd@gmail.com>
2024-01-15 11:12:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import os
+
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+import time
+import os
+
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
+
+# Optional - Keep the browser open (helps diagnose issues if the script crashes)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3759787458&distance=100&f_AL=true&f_SB2=42&geoId=102681496&keywords=Application%20Support&location=Glasgow%2C%20Scotland%2C%20United%20Kingdom&origin=JOB_SEARCH_PAGE_JOB_FILTER&refresh=true")
+
+# Click Reject Cookies Button
+time.sleep(2)
+reject_button = driver.find_element(by=By.CSS_SELECTOR, value='button[action-type="DENY"]')
+reject_button.click()
+
+# Click Sign in Button
+time.sleep(2)
+sign_in_button = driver.find_element(by=By.LINK_TEXT, value="Sign in")
+sign_in_button.click()
+
+# Sign in
+time.sleep(5)
+search_email = driver.find_element(By.NAME, value="session_key")
+search_password = driver.find_element(By.NAME, value="session_password")
+search_email.send_keys(EMAIL)
+search_password.send_keys(PASSWORD, Keys.ENTER)
+
+# You may be presented with a CAPTCHA - Solve the Puzzle Manually
+input("Press Enter when you have solved the Captcha")
+
+time.sleep(5)
+easy_apply = driver.find_element(By.LINK_TEXT, value="Easy Apply")
+easy_apply.send_keys(Keys.ENTER)
+
+time.sleep(5)
+next_button = driver.find_element(By.LINK_TEXT, value="Next")
+next_button.send_keys(Keys.ENTER)
+
+time.sleep(5)
+next_button.send_keys(Keys.ENTER)
+
+time.sleep(5)
+review_button = driver.find_element(By.LINK_TEXT, value="Review")
+review_button.send_keys(Keys.ENTER)
+
+time.sleep(5)
+submit_button = driver.find_element(By.LINK_TEXT, value="Submit application")
+submit_button.send_keys(Keys.ENTER)
